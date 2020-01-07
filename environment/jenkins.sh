331b2eadaaec85bb610fc0a55768c9addcd7315f
Dilan Morar <DMorar@spartaglobal.com>
2020-01-07 15:35:50 +0000

    jenkins.sh file created and hashed out previous code

--- environment/jenkins.sh
+++ environment/jenkins.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-
-sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
-
-sudo apt-get update
-sudo apt-get upgrade
-
-sudo apt install default-jre -y
-
-sudo apt-get install -y jenkins
-
-sudo apt-get install python-software-properties -y
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install nodejs -y
-
-sudo npm install pm2 -g
-
-sudo apt-get install nginx -y
-
 # cat var/log/jenkins/jenkins.log
 
 # echo "Adding apt-keys"
